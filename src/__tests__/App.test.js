7d52c21d95530ae7a37524b0cd201fe09e86be6a
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-10-30 22:38:16 +0300

    initial commit

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,3 +4,16 @@ import App from "../App";
 import '@testing-library/jest-dom';
 
 // Code tests here
+//  test initial state of the page
+test("pizza checkbox is initially unchecked", () => {
+    render(<App />);
+    const  addPeperoni = screen.lgetByRole("checkbox", { name: /add peperoni/i });
+    expect(addPeperoni).not.toBeChecked();
+})
+
+test("toppings list initially contains only cheese", () => {})
+
+// test the effect of  clicking the checkbox 
+
+
+// test the effect of  clicking the chheckbox a second time
